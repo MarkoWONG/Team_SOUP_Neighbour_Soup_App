1721db1954e2552d814cb8ce31a0553d2f950486
Manyu Duan <z5259564@unsw.edu.au>
2022-11-10 14:53:12 +1100

    add title and font details

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import styles from './Styles.js';
+import styles from './AppStyles.js';
 import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! >:D</Text>
+      <Text style={styles.title} >Open up App.js to start working on your app! >:D</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
