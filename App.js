ffb20d67048c003c9dd88f783b9d63e17093c511
MarkoWONG <Nfslolmiku@gmail.com>
2022-11-12 22:35:53 +1100

    added very basic buy and sell pages

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import styles from './AppStyles.js';
-import { Text, View } from 'react-native';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Fontisto } from '@expo/vector-icons';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Marketplace from "./pages/marketplace/Buy";
+import Sell from "./pages/marketplace/Sell";
+import Buy from "./pages/marketplace/Buy";
+
+const RootStack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
+  const TabsNav = () => (
+    <Tabs.Navigator screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="Marketplace"
+        component={Marketplace}
+        options={{
+          tabBarIcon: ({ size }) => <Fontisto name="shopping-store" size={size} color="black" />,
+        }}
+      />
+      {/* <Tabs.Screen
+        name="Profile"
+        component={account}
+        options={{
+          tabBarIcon: ({ size }) => <Ionicons name="md-settings" size={size} />,
+        }}
+      /> */}
+    </Tabs.Navigator>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title} >Open up App.js to start working on your app! >:D</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator initialRouteName="BottomTabs">
+        <RootStack.Screen
+          name="BottomTabs"
+          component={TabsNav}
+          options={{ headerShown: false }}
+        />
+        <RootStack.Screen
+          name="Sell"
+          component={Sell}
+          options={{animation: 'none', headerShown: false, }}
+        />
+        <RootStack.Screen
+          name="Buy"
+          component={Buy}
+          options={{animation: 'none',  headerShown: false,}}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
-
