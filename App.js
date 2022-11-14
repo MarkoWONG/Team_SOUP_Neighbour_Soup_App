9b7e122c77c9febc0ec35bca3cd0eb743a7da5bb
mck-jacky <machakki23@gmail.com>
2022-11-14 23:51:15 +1100

    finished

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import styles from './AppStyles.js';
-import { Text, View } from 'react-native';
-import Signup from './pages/signin_signup/signup.js';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Postcode from './pages/signin_signup/Postcode.js';
+import Signin from './pages/signin_signup/Signin.js'
+import Signup from './pages/signin_signup/Signup.js'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text style={styles.title} >Open up App.js to start working on your app! >:D</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <Signup />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Signup" 
+          component={Signup} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="Signin" 
+          component={Signin} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Postcode"
+          component={Postcode}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
