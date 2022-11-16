ca353e6861ea04d2fb7debca82b3c7e4af470654
Manyu Duan <z5259564@unsw.edu.au>
2022-11-16 19:51:15 +1100

    save

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import styles from './AppStyles.js';
+import styles from './App.styles.js';
 import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title} >Open up App.js to start working on your app! >:D</Text>
+      <Text style={styles.title} >Welcome to NeighbourSoup!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Navbar.styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    baseText: {
+        fontFamily: 'FrancoisOne',
+        fontSize: 10,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#6BB972',
+        alignItems: 'flex',
+        justifyContent: 'center',
+    },
+    contentContainer: {
+        flex: 1,
+    },
+    title: {
+        fontFamily: 'FrancoisOne',
+        fontSize: 45,
+        fontWeight: 'bold',
+        color: '#6BB972',
+    }
+});
+
+export default styles;
