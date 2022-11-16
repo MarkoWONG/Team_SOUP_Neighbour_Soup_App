ebf50b97b9ff59908e12cbf7234d192b68fe68de
Manyu Duan <z5259564@unsw.edu.au>
2022-11-17 07:55:05 +1100

    save again

--- App.js
+++ App.js
@@ -3,8 +3,30 @@ import styles from './App.styles.js';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+const RootStack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
+  const TabsNav = () => (
+    <Tabs.Navigator screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="Todo"
+        component={Todo}
+        options={{
+          tabBarIcon: ({ size }) => <Ionicons name="md-list" size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarIcon: ({ size }) => <Ionicons name="md-settings" size={size} />,
+        }}
+      />
+    </Tabs.Navigator>
+  );
+
+
   return (<NavigationContainer>
     <View style={styles.container}>
       <Text style={styles.title} >Welcome to NeighbourSoup!</Text>
