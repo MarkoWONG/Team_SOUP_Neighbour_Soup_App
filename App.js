08a28b23adb4c456098f1735e8c9094d6f7fb03b
Manyu Duan <z5259564@unsw.edu.au>
2022-11-17 07:54:37 +1100

    save

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import styles from './App.styles.js';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
-  return (
+  return (<NavigationContainer>
     <View style={styles.container}>
       <Text style={styles.title} >Welcome to NeighbourSoup!</Text>
       <StatusBar style="auto" />
     </View>
+  </NavigationContainer>
   );
 }
 
