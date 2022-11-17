c3b1c8ce5e257f2c1a86194aa63f1630071b20fd
Manyu Duan <z5259564@unsw.edu.au>
2022-11-17 13:20:58 +1100

    add map

--- App.js
+++ App.js
@@ -2,31 +2,33 @@ import { Fontisto, FontAwesome  } from '@expo/vector-icons';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Marketplace from "./pages/marketplace/Buy";
-import Sell from "./pages/marketplace/Sell";
-import Buy from "./pages/marketplace/Buy";
-import CreateListing from "./pages/marketplace/CreateListing";
-import ListingDetails from './pages/marketplace/ListingDetails';
-import EditListing from './pages/marketplace/EditListing';
-import Profile from './pages/profile/Profile';
-import EditProfile from './pages/profile/Editprofile';
+import Home from './pages/home/home.js';
+import Buy from "./pages/marketplace/Buy.js";
+import Sell from "./pages/marketplace/Sell.js";
+import CreateListing from "./pages/marketplace/CreateListing.js";
+import ListingDetails from './pages/marketplace/ListingDetails.js';
+import EditListing from './pages/marketplace/EditListing.js';
+import Profile from './pages/profile/Profile.js';
+import EditProfile from './pages/profile/Editprofile.js';
 import Postcode from './pages/signin_signup/Postcode.js';
 import Signin from './pages/signin_signup/Signin.js'
 import Signup from './pages/signin_signup/Signup.js'
-import Tutorial from './pages/signin_signup/Tutorial';
+import Tutorial from './pages/signin_signup/Tutorial.js';
+
+
 const RootStack = createNativeStackNavigator();
 const Tabs = createBottomTabNavigator();
 
 export default function App() {
     const TabsNav = () => (
         <Tabs.Navigator screenOptions={{ headerShown: false }}>
-        <Tabs.Screen
-            name="Marketplace"
-            component={Marketplace}
+        {/* <Tabs.Screen
+            name="Buy"
+            component={Buy}
             options={{
             tabBarIcon: ({ size }) => <Fontisto name="shopping-store" size={size} color="black" />,
             }}
-        />
+        /> */}
         <Tabs.Screen
             name="Profile"
             component={Profile}
@@ -41,13 +43,20 @@ export default function App() {
             tabBarIcon: ({ size }) => <FontAwesome name="user-circle-o" size={size} color="black" />,
             }}
         />
+        <Tabs.Screen
+            name="Home"
+            component={Home}
+            options={{
+                tabBarIcon: ({ size }) => <FontAwesome name="user-circle-o" size={size} color="black" />,
+            }}
+        />
         </Tabs.Navigator>
     );
 
     return (
         <NavigationContainer>
-            <RootStack.Navigator initialRouteName="Signup">
-                <RootStack.Screen
+            <RootStack.Navigator initialRouteName="BottomTabs">    
+            <RootStack.Screen
                 name="BottomTabs"
                 component={TabsNav}
                 options={{ headerShown: false }}
@@ -117,6 +126,11 @@ export default function App() {
                 name="Tutorial"
                 component={Tutorial}
                 options={{ headerShown: false, }}
+                />
+                <RootStack.Screen
+                name="Home"
+                component={Home}
+                options={{animation: 'none',  headerShown: false,}}
                 />
             </RootStack.Navigator>
         </NavigationContainer>
